dfb2a0ccf90e037b2122326ccfc2b73fb557baf1
JoeyKCS <106928210+JoeyKCS@users.noreply.github.com>
2023-03-10 13:24:16 +0200

    Update Header.tsx

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -24,39 +24,30 @@ export const Header = () => {
           <p >
             I'm a <span className="font-weight-bold text-info">Junior Web Developer</span> currently in Pretoria, Centurion. I have a keen interest in all things related to Web and Software development. Aiming to improve the IT space with interactive designs.
           </p>
-        <Link
-       
-        href="/contactPage"
-        className="btn btn-primary rounded text-white text-xl font-semibold px-4 py-2 shadow-sm hover:bg-teal-700 mr-4"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-100}
-        duration={500}>
-          Contact
-
-        </Link>
-        </div>
-        
-       <div className="d-flex justify-content-center mt-4">
-  <Link href="/about" >
-    <a>About Me</a>
-    <div style={{ borderBottom: '8px solid blue' }}>
+          <Link href="/contactPage">
+            <div className="btn btn-primary rounded text-white text-xl font-semibold px-4 py-2 shadow-sm hover:bg-teal-300 mt-4">
+              Contact
+            </div>
+          </Link>
+          <a href="#about" className={styles['scroll-to-section']} onClick={(e) => {
+  e.preventDefault();
+  const target = document.querySelector("#about");
+  target.scrollIntoView({ behavior: "smooth", block: "start" });
+}}>
+  <div style={{ borderBottom: '6px solid blue'  }}>
     <br />
     <HiArrowDown
       size={35}
       className={styles['animate-bounce']}
-   
-   />
+    />
   </div>
-  </Link>
- </div>
-            
-           
-          
+</a>
+        
 
  
- </div>
+        
+</div>
+</div>         
  </section>
 
   )
