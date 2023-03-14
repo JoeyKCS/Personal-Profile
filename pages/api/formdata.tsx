9c201f127b6edf661352f927bb7ac52a754268ba
JoeyKCS <106928210+JoeyKCS@users.noreply.github.com>
2023-03-14 17:09:31 +0200

    Update formdata.tsx

--- pages/api/formdata.tsx
+++ pages/api/formdata.tsx
@@ -1,3 +1,28 @@
+const handleSubmit = async (event) => {
+    event.preventDefault();
+  
+    const formData = {
+      firstName: event.target.firstName.value,
+      lastName: event.target.lastName.value,
+      subject: event.target.subject.value,
+      message: event.target.message.value,
+      email: event.target.email.value,
+      phone: event.target.phone.value,
+    };
+  
+    try {
+      const response = await axios.post(process.env.API_ENDPOINT, formData, {
+        headers: { Authorization: `Bearer ${process.env.API_KEY}` },
+      });
+      console.log('response', response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+
+
 export default function handler(req,res){
     const body = req.body;
 
